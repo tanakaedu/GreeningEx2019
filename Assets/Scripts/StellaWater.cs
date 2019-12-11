6be180e08eb96a9bb55107c1b0a9488f35bde8c4
satoshigp <satoshi3104>
2019-12-11 09:06:31 +0900

    アニメーション

---
+++ Assets/Scripts/StellaWater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StellaWater : MonoBehaviour
+{
+    [Tooltip("水プレハブ"),SerializeField]
+    GameObject waterPrefab = null;
+
+    Camera mainCamera;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mpos = Input.mousePosition;
+            mpos.z = 0f - mainCamera.transform.position.z;
+
+            Vector3 wpos = mainCamera.ScreenToWorldPoint(mpos);
+
+            Instantiate(waterPrefab, wpos, Quaternion.identity);
+        }
+    }
+}
